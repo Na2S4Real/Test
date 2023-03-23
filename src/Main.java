e46624b486045f051a2e235b4771f5c7a527db09
Na2S4Real <118374052+Na2S4Real@users.noreply.github.com>
2023-03-23 12:48:14 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class test
+{
+    public static void main(String[] args) {
+        int agePersonne = 19;
+        agePersonne = 33;
+
+        System.out.printin("Age = " + agePersonne + " ans");
+    }
+}
+
+
+
